3e10c573c10e603fe4ba2b4d11fab15180131a37
Colin Allman <colinallmangame@gmail.com>
2020-04-17 18:00:05 -0500

    microsplat basic setup

---
+++ Ludum2020Project/Ludum2020/Assets/MicroSplat/ProceduralTexture/MicroSplatProceduralTextureConfig.cs
@@ -0,0 +1,332 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MicroSplatProceduralTextureConfig : ScriptableObject
+{
+   public enum TableSize
+   {
+      k64 = 64,
+      k128 = 128,
+      k256 = 256,
+      k512 = 512,
+      k1024 = 1024,
+      k2048 = 2048,
+      k4096 = 4096
+   }
+
+   public TableSize proceduralCurveTextureSize = TableSize.k256;
+
+   [System.Serializable]
+   public class Layer
+   {
+      public float weight = 1;
+      public int textureIndex = 0;
+      public bool noiseActive;
+      public float noiseFrequency = 1;
+      public float noiseOffset = 0;
+      public Vector2 noiseRange = new Vector2(0, 1);
+
+      public Vector4 biomeWeights = new Vector4(1, 1, 1, 1);
+      public Vector4 biomeWeights2 = new Vector4 (1, 1, 1, 1);
+
+      public bool heightActive;
+      public AnimationCurve heightCurve = AnimationCurve.Linear(0, 1, 1, 1);
+      public bool slopeActive;
+      public AnimationCurve slopeCurve = AnimationCurve.Linear(0, 1, 1, 1);
+      public bool erosionMapActive;
+      public AnimationCurve erosionMapCurve = AnimationCurve.Linear(0, 1, 1, 1);
+      public bool cavityMapActive;
+      public AnimationCurve cavityMapCurve = AnimationCurve.Linear(0, 1, 1, 1);
+
+      
+
+      public Layer Copy()
+      {
+         Layer l = new Layer ();
+         l.weight = weight;
+         l.textureIndex = textureIndex;
+         l.noiseActive = noiseActive;
+         l.noiseFrequency = noiseFrequency;
+         l.noiseOffset = noiseOffset;
+         l.noiseRange = noiseRange;
+         l.biomeWeights = biomeWeights;
+         l.biomeWeights2 = biomeWeights2;
+         l.heightActive = heightActive;
+         l.slopeActive = slopeActive;
+         l.erosionMapActive = erosionMapActive;
+         l.cavityMapActive = cavityMapActive;
+         l.heightCurve = new AnimationCurve (heightCurve.keys);
+         l.slopeCurve = new AnimationCurve (slopeCurve.keys);
+         l.erosionMapCurve = new AnimationCurve (erosionMapCurve.keys);
+         l.cavityMapCurve = new AnimationCurve (cavityMapCurve.keys);
+         return l;
+      }
+   }
+
+   [System.Serializable]
+   public class HSVCurve
+   {
+      public AnimationCurve H = AnimationCurve.Linear(0, 0.5f, 1, 0.5f);
+      public AnimationCurve S = AnimationCurve.Linear(0, 0.0f, 1, 0.0f);
+      public AnimationCurve V = AnimationCurve.Linear(0, 0.0f, 1, 0.0f);
+   }
+
+
+   public List<Gradient> heightGradients = new List<Gradient>();
+   public List<HSVCurve> heightHSV = new List<HSVCurve>();
+   public List<Gradient> slopeGradients = new List<Gradient> ();
+   public List<HSVCurve> slopeHSV = new List<HSVCurve> ();
+
+
+   [HideInInspector]
+   public List<Layer> layers = new List<Layer>();
+
+
+   // cached textures, generated on demand when Syncing
+   Texture2D curveTex;
+   Texture2D paramTex;
+   Texture2D heightGradientTex;
+   Texture2D heightHSVTex;
+   Texture2D slopeGradientTex;
+   Texture2D slopeHSVTex;
+
+
+   // 32, 128 LUT for curves. R = Height, G = Slope
+   public Texture2D GetHeightGradientTexture()
+   {
+      int height = 32;  // max layers
+      int width = 128;
+      if (heightGradientTex == null)
+      {
+         heightGradientTex = new Texture2D(width, height, TextureFormat.RGBA32, false);
+         heightGradientTex.hideFlags = HideFlags.HideAndDontSave;
+      }
+
+      Color grey = Color.grey;
+      for (int i = 0; i < heightGradients.Count; ++i)
+      {
+         for (int x = 0; x < width; ++x)
+         {
+            Color c = grey;
+            float v = (float)x / width;
+            c = heightGradients[i].Evaluate(v);
+            heightGradientTex.SetPixel(x, i, c);
+         }
+      }
+      for (int i = heightGradients.Count; i < 32; ++i)
+      {
+         for (int x = 0; x < width; ++x)
+         {
+            heightGradientTex.SetPixel(x, i, grey);
+         }
+      }
+
+
+      heightGradientTex.Apply(false, false);
+      return heightGradientTex;
+   }
+
+
+   // 32, 128 LUT for HSV curves
+   public Texture2D GetHeightHSVTexture()
+   {
+      int height = 32;  // max layers
+      int width = 128;
+      if (heightHSVTex == null)
+      {
+         heightHSVTex = new Texture2D(width, height, TextureFormat.RGBA32, false);
+         heightHSVTex.hideFlags = HideFlags.HideAndDontSave;
+      }
+
+      Color grey = Color.grey;
+      for (int i = 0; i < heightHSV.Count; ++i)
+      {
+         for (int x = 0; x < width; ++x)
+         {
+            Color c = grey;
+            float v = (float)x / width;
+            c.r = heightHSV[i].H.Evaluate(v) * 0.5f + 0.5f;
+            c.g = heightHSV[i].S.Evaluate(v) * 0.5f + 0.5f;
+            c.b = heightHSV[i].V.Evaluate(v) * 0.5f + 0.5f;
+            heightHSVTex.SetPixel(x, i, c);
+         }
+      }
+      for (int i = heightHSV.Count; i < 32; ++i)
+      {
+         for (int x = 0; x < width; ++x)
+         {
+            heightHSVTex.SetPixel(x, i, grey);
+         }
+      }
+
+
+      heightHSVTex.Apply(false, false);
+      return heightHSVTex;
+   }
+
+   // 32, 128 LUT for curves. R = Height, G = Slope
+   public Texture2D GetSlopeGradientTexture ()
+   {
+      int height = 32;  // max layers
+      int width = 128;
+      if (slopeGradientTex == null)
+      {
+         slopeGradientTex = new Texture2D (width, height, TextureFormat.RGBA32, false);
+         slopeGradientTex.hideFlags = HideFlags.HideAndDontSave;
+      }
+
+      Color grey = Color.grey;
+      for (int i = 0; i < slopeGradients.Count; ++i)
+      {
+         for (int x = 0; x < width; ++x)
+         {
+            Color c = grey;
+            float v = (float)x / width;
+            c = slopeGradients [i].Evaluate (v);
+            slopeGradientTex.SetPixel (x, i, c);
+         }
+      }
+      for (int i = slopeGradients.Count; i < 32; ++i)
+      {
+         for (int x = 0; x < width; ++x)
+         {
+            slopeGradientTex.SetPixel (x, i, grey);
+         }
+      }
+
+
+      slopeGradientTex.Apply (false, false);
+      return slopeGradientTex;
+   }
+
+
+   // 32, 128 LUT for HSV curves
+   public Texture2D GetSlopeHSVTexture ()
+   {
+      int height = 32;  // max layers
+      int width = 128;
+      if (slopeHSVTex == null)
+      {
+         slopeHSVTex = new Texture2D (width, height, TextureFormat.RGBA32, false);
+         slopeHSVTex.hideFlags = HideFlags.HideAndDontSave;
+      }
+
+      Color grey = Color.grey;
+      for (int i = 0; i < slopeHSV.Count; ++i)
+      {
+         for (int x = 0; x < width; ++x)
+         {
+            Color c = grey;
+            float v = (float)x / width;
+            c.r = slopeHSV [i].H.Evaluate (v) * 0.5f + 0.5f;
+            c.g = slopeHSV [i].S.Evaluate (v) * 0.5f + 0.5f;
+            c.b = slopeHSV [i].V.Evaluate (v) * 0.5f + 0.5f;
+            slopeHSVTex.SetPixel (x, i, c);
+         }
+      }
+      for (int i = slopeHSV.Count; i < 32; ++i)
+      {
+         for (int x = 0; x < width; ++x)
+         {
+            slopeHSVTex.SetPixel (x, i, grey);
+         }
+      }
+
+
+      slopeHSVTex.Apply (false, false);
+      return slopeHSVTex;
+   }
+
+   // 32, tableSize LUT for curves. R = Height, G = Slope, B = Cavity, A = Erosion
+   public Texture2D GetCurveTexture()
+   {
+      int height = 32;  // max layers
+      int width = (int)proceduralCurveTextureSize;
+      if (curveTex != null && curveTex.width != width)
+      {
+         DestroyImmediate (curveTex);
+         curveTex = null;
+      }
+      if (curveTex == null)
+      {
+         curveTex = new Texture2D(width, height, TextureFormat.RGBA32, false, true);
+         curveTex.hideFlags = HideFlags.HideAndDontSave;
+      }
+      
+
+      Color white = Color.white;
+      for (int i = 0; i < layers.Count; ++i)
+      {
+         for (int x = 0; x < width; ++x)
+         {
+            Color c = white;
+            float v = (float)x / width;
+            if (layers[i].heightActive)
+            {
+               c.r = layers[i].heightCurve.Evaluate(v);
+            }
+            if (layers[i].slopeActive)
+            {
+               c.g = layers[i].slopeCurve.Evaluate(v);
+            }
+            if (layers[i].cavityMapActive)
+            {
+               c.b = layers[i].cavityMapCurve.Evaluate(v);
+            }
+            if (layers[i].erosionMapActive)
+            {
+               c.a = layers[i].erosionMapCurve.Evaluate(v);
+            }
+            curveTex.SetPixel(x, i, c);
+         }
+      }
+
+
+      curveTex.Apply(false, false);
+      return curveTex;
+   }
+
+
+
+   // 4x32 LUT, noise aprams in x0 (RGBA), weights in x1.r
+   public Texture2D GetParamTexture()
+   {
+      int height = 32;  // max textures
+      int width = 4;
+      if (paramTex == null || paramTex.format != TextureFormat.RGBAHalf || paramTex.width != width)
+      {
+         paramTex = new Texture2D(width, height, TextureFormat.RGBAHalf, false, true);
+         paramTex.hideFlags = HideFlags.HideAndDontSave;
+      }
+
+
+      Color black = new Color(0, 0, 0, 0);
+      for (int i = 0; i < layers.Count; ++i)
+      {
+         Color c0 = black;
+         Color c1 = black;
+         if (layers[i].noiseActive)
+         {
+            c0.r = layers[i].noiseFrequency;
+            c0.g = layers[i].noiseRange.x;
+            c0.b = layers[i].noiseRange.y;
+            c0.a = layers[i].noiseOffset;
+         }
+         c1.r = layers[i].weight;
+         c1.g = layers[i].textureIndex;
+         paramTex.SetPixel(0, i, c0);
+         paramTex.SetPixel(1, i, c1);
+         Vector4 bw = layers[i].biomeWeights;
+         paramTex.SetPixel(2, i, new Color(bw.x, bw.y, bw.z, bw.w));
+         Vector4 bw2 = layers [i].biomeWeights2;
+         paramTex.SetPixel (3, i, new Color (bw2.x, bw2.y, bw2.z, bw2.w));
+      }
+
+      paramTex.Apply(false, false);
+      return paramTex;
+   }
+
+}
+
---
+++ Ludum2020Project/Ludum2020/Assets/MicroSplat/ProceduralTexture/MicroSplatProceduralTextureUtil.cs
@@ -0,0 +1,190 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// for sampling texturing from the CPU. If your using jobs, there's a jobified version
+// included in a zip file next to this version.
+
+namespace JBooth.MicroSplat
+{
+   public class MicroSplatProceduralTextureUtil
+   {
+
+
+      public enum NoiseUVMode
+      {
+         UV = 0,
+         World,
+         Triplanar
+      }
+
+      static float PCFilter(int index, float height, float slope, float cavity, float flow, Vector3 worldPos, Vector2 uv,
+          Color bMask, out int texIndex, Vector3 pN,
+          Texture2D procTexCurves, Texture2D procTexParams, Texture2D procTexNoise, NoiseUVMode noiseMode)
+      {
+         // params0 are rgba (noise scale, min, max, offset)
+         // params1 are rg (weight, index)
+         Vector2 noiseUV = uv;
+
+         float offset = 1.0f / 32.0f;
+         float halfOff = 1.0f / 64.0f;
+         float y = (index * offset) + halfOff;
+
+         float h0 = procTexCurves.GetPixelBilinear(height, y).r;
+         float s0 = procTexCurves.GetPixelBilinear(slope, y).g;
+         float c0 = procTexCurves.GetPixelBilinear(cavity, y).b;
+         float f0 = procTexCurves.GetPixelBilinear(flow, y).a;
+
+         Color params0 = procTexParams.GetPixelBilinear(0.166666f, y);
+         Color params1 = procTexParams.GetPixelBilinear(0.5f, y);
+         Color params2 = procTexParams.GetPixelBilinear(0.833333f, y);
+
+
+         Color noise = new Color(0, 0, 0, 0);
+         if (noiseMode == NoiseUVMode.Triplanar)
+         {
+            Vector2 nUV0 = new Vector2(worldPos.z, worldPos.y) * 0.002f * params0.r + new Vector2(params0.a, params0.a);
+            Vector2 nUV1 = new Vector2(worldPos.x, worldPos.z) * 0.002f * params0.r + new Vector2(params0.a + 0.31f, params0.a + 0.31f);
+            Vector2 nUV2 = new Vector2(worldPos.x, worldPos.y) * 0.002f * params0.r + new Vector2(params0.a + 0.71f, params0.a + 0.71f);
+
+            Color noise0 = procTexNoise.GetPixelBilinear(nUV0.x, nUV0.y);
+            Color noise1 = procTexNoise.GetPixelBilinear(nUV1.x, nUV1.y);
+            Color noise2 = procTexNoise.GetPixelBilinear(nUV2.x, nUV2.y);
+            noise = noise0 * pN.x + noise1 * pN.y + noise2 * pN.z;
+         }
+         else if (noiseMode == NoiseUVMode.World)
+         {
+            noise = procTexNoise.GetPixelBilinear(noiseUV.x * params0.r + params0.a, noiseUV.y * params0.r + params0.a);
+         }
+         else if (noiseMode == NoiseUVMode.UV)
+         {
+            noise *= procTexNoise.GetPixelBilinear(worldPos.x * 0.002f * params0.r + params0.a, worldPos.z * 0.002f * params0.r + params0.a);
+         }
+
+         // unpack
+         noise.r = noise.r * 2 - 1;
+         noise.g = noise.g * 2 - 1;
+
+         h0 *= 1.0f + Mathf.Lerp(params0.g, params0.b, noise.r);
+         s0 *= 1.0f + Mathf.Lerp(params0.g, params0.b, noise.g);
+         c0 *= 1.0f + Mathf.Lerp(params0.g, params0.b, noise.b);
+         f0 *= 1.0f + Mathf.Lerp(params0.g, params0.b, noise.a);
+         float bWeight = 1;
+         bMask *= params2;
+         // handle 16 mode
+         bWeight = Mathf.Max(Mathf.Max(Mathf.Max(bMask.r, bMask.g), bMask.b), bMask.a);
+
+         texIndex = (int)params1.g;
+         return Mathf.Clamp01(h0 * s0 * c0 * f0 * params1.r * bWeight);
+      }
+
+      public struct Int4
+      {
+         public int x;
+         public int y;
+         public int z;
+         public int w;
+      }
+
+      static void PCProcessLayer(ref Vector4 weights, ref Int4 indexes, ref float totalWeight,
+          int curIdx, float height, float slope, float cavity, float flow, Vector3 worldPos, Vector2 uv,
+          Color biomeMask, Vector3 pN,
+          Texture2D procTexCurves, Texture2D procTexParams, Texture2D procTexNoise, NoiseUVMode noiseMode)
+      {
+         int texIndex = 0;
+         float w = PCFilter(curIdx, height, slope, cavity, flow, worldPos, uv, biomeMask, out texIndex, pN, procTexCurves, procTexParams, procTexNoise, noiseMode);
+         w = Mathf.Min(totalWeight, w);
+         totalWeight -= w;
+
+         // sort
+         if (w > weights.x)
+         {
+            weights.w = weights.z;
+            weights.z = weights.y;
+            weights.y = weights.x;
+            indexes.w = indexes.z;
+            indexes.z = indexes.y;
+            indexes.y = indexes.x;
+            weights.x = w;
+            indexes.x = texIndex;
+         }
+         else if (w > weights.y)
+         {
+            weights.w = weights.z;
+            weights.z = weights.y;
+            indexes.w = indexes.z;
+            indexes.z = indexes.y;
+            weights.y = w;
+            indexes.y = texIndex;
+         }
+         else if (w > weights.z)
+         {
+            weights.w = weights.z;
+            indexes.w = indexes.z;
+            weights.z = w;
+            indexes.z = texIndex;
+         }
+         else if (w > weights.w)
+         {
+            weights.w = w;
+            indexes.w = texIndex;
+         }
+      }
+
+
+      public static void Sample(Vector2 uv, Vector3 worldPos, int layerCount, float worldHeight, Vector2 worldRange, Vector3 worldNormal, Vector3 up,
+      Texture2D curves, Texture2D properties, Texture2D cavityMap, Texture2D noise, Texture2D mask, bool biom16mode, NoiseUVMode noiseUVMode, out Vector4 weights, out Int4 indexes)
+      {
+
+         weights = new Vector4(0, 0, 0, 0);
+
+         float height = Mathf.Clamp01((worldHeight - worldRange.x) / Mathf.Max(0.1f, (worldRange.y - worldRange.x)));
+         float slope = 1.0f - Mathf.Clamp01(Vector3.Dot(worldNormal, up) * 0.5f + 0.49f);
+         float cavity = 0.5f;
+         float flow = 0.5f;
+         if (cavityMap != null)
+         {
+            var p = cavityMap.GetPixelBilinear(uv.x, uv.y);
+            cavity = p.g;
+            flow = p.a;
+         }
+         // find 4 highest weights and indexes
+         indexes = new Int4();
+         indexes.x = 0;
+         indexes.y = 1;
+         indexes.z = 2;
+         indexes.w = 3;
+
+         float totalWeight = 1.0f;
+
+         Color biomeMask = new Color(1, 1, 1, 1);
+         if (mask != null)
+         {
+            biomeMask = mask.GetPixelBilinear(uv.x, uv.y);
+         }
+
+         Vector3 pN = new Vector3(0, 0, 0);
+         if (noiseUVMode == NoiseUVMode.Triplanar)
+         {
+            Vector3 absWN = worldNormal;
+            absWN.x = Mathf.Abs(absWN.x);
+            absWN.y = Mathf.Abs(absWN.y);
+            absWN.z = Mathf.Abs(absWN.z);
+            pN.x = Mathf.Pow(absWN.x, 4);
+            pN.y = Mathf.Pow(absWN.y, 4);
+            pN.z = Mathf.Pow(absWN.z, 4);
+            float ttl = pN.x + pN.y + pN.z;
+            pN.x /= ttl;
+            pN.y /= ttl;
+            pN.z /= ttl;
+         }
+
+
+         for (int i = 0; i < layerCount; ++i)
+         {
+            PCProcessLayer(ref weights, ref indexes, ref totalWeight, i, height, slope, cavity, flow, worldPos, uv, biomeMask, pN, curves, properties, noise, noiseUVMode);
+         }
+
+      }
+   }
+}
+
