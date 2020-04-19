79117a366b69bc4c18259fd4db9e7658e22d8fee
JoshuaW421 <joshuawillmon@gmail.com>
2020-04-19 13:31:27 -0700

    leaf particle system

---
+++ Ludum2020Project/Ludum2020/Assets/ParticleTest/DebrisDestroy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebrisDestroy : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("DebrisBurn"))
+        {
+            Destroy(this.gameObject);
+           
+        }
+    }
+    public float debrisLifeTime = 10f;
+    private void Start()
+    {
+        Destroy(gameObject, debrisLifeTime);
+    }
+
+
+}
---
+++ Ludum2020Project/Ludum2020/Assets/ParticleTest/DebrisSpawn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebrisSpawn : MonoBehaviour
+{
+
+    public Transform leafprefab;
+    public float leafSpeed = 5f;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 3; i++)
+            Instantiate(leafprefab, new Vector3(Random.Range(-6, 6), 8, 2), Quaternion.identity);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+        
+    {
+
+       
+    }
+}
