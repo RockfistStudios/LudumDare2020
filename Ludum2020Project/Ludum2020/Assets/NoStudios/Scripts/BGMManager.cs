032390fd47211036c462360ca30558b2340f1640
ColinAllmanGame <colinallmangame@gmail.com>
2020-04-19 20:28:00 -0500

    prefabs

---
+++ Ludum2020Project/Ludum2020/Assets/NoStudios/Scripts/BGMManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DarkTonic.MasterAudio;
+
+public class BGMManager : MonoBehaviour
+{
+    public PlaylistController PC;
+    float opv = 0f;
+    public void StartIntro()
+    {
+        opv = PC.PlaylistVolume;
+        PC.StartPlaylist("Toasted");
+    }
+    public void FadeIntroDown()
+    {
+        PC.StopPlaylist();
+    }
+    public void ResumeLoop()
+    {
+        PC.PlayNextSong();
+    }
+
+    public void SetLoopByHealth(int level)
+    {
+
+    }
+
+
+}
